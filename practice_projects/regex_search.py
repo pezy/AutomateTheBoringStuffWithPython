ad950d46c142d01b56afb72e808980cde640bf0d
kerogen-pezy <urbancpz@gmail.com>
2017-06-20 11:14:34 +0800

    finished project of chapter 8

--- practice_projects/regex_search.py
+++ practice_projects/regex_search.py
@@ -6,9 +6,16 @@ regex_search.py - opens all .txt files in a folder and searches for any line
 '''
 
 import os
+import re
 
 target_folder = input("Enter the target foulder:\n")
 os.chdir(target_folder)
 target_files = os.listdir(target_folder)
 target_files = [target for target in target_files if target.endswith('.txt')]
-print(target_files)
+regex = re.compile(input('Enter the regular expression:\n'))
+for file_name in target_files:
+    file = open(file_name)
+    for line in file.readlines():
+        words = regex.findall(line)
+        if words:
+            print(file_name + ": " + str(words))
